d40e47c2527d0943d37637c82d42f261d0f8159f
KW <k.wojtasinski@wp.pl>
2022-10-22 22:24:51 +0200

    [MyOrders] Frontend: Loaders styles, css, scss & Sass Compiling

---
+++ src/Frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js' // name of entry as an output generated js
+    },
+    module: {
+        rules: [
+            {
+                // any file with extension scss
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
