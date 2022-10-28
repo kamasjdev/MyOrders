642ab4c88dffdfe327d74c63d49a5602c0a0d997
KW <k.wojtasinski@wp.pl>
2022-10-29 01:03:53 +0200

    [MyOrders] Backend: IntegrationTests
    Added integration tests

---
+++ tests/MyOrders.IntegrationTests/Controllers/ProductController.cs
@@ -0,0 +1,127 @@
+﻿using Flurl.Http;
+using MyOrders.Application.DTO;
+using MyOrders.IntegrationTests.Common;
+using Shouldly;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MyOrders.IntegrationTests.Controllers
+{
+    public class ProductController : BaseControllerTest, IAsyncLifetime
+    {
+        [Fact]
+        public async Task should_add_product_and_return_201()
+        {
+            var productKindDto = _productKinds.First();
+            var dto = new ProductDetailsDto(0, "Name", productKindDto, 120);
+
+            var response = await Client.Request(Path).PostJsonAsync(dto);
+
+            response.StatusCode.ShouldBe((int)HttpStatusCode.Created);
+            var productAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductDetailsDto>();
+            productAdded.ProductName.ShouldBe(dto.ProductName);
+            productAdded.Price.ShouldBe(dto.Price);
+            productAdded.ProductKind.Id.ShouldBe(dto.ProductKind.Id);
+            productAdded.ProductKind.ProductKindName.ShouldBe(dto.ProductKind.ProductKindName);
+        }
+
+        [Fact]
+        public async Task should_update_product_and_return_200()
+        {
+            var productAdded = await AddDefaultProduct();
+            var dto = new ProductDetailsDto(productAdded.Id, "ABCDaef", _productKinds.OrderByDescending(pk => pk.Id).First(), 99);
+
+            var productKindUpdatedResponse = await Client.Request($"{Path}/{productAdded.Id}").PutJsonAsync(dto);
+
+            productKindUpdatedResponse.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+            var productUpdated = await productKindUpdatedResponse.ResponseMessage.Content.ReadFromJsonAsync<ProductDetailsDto>();
+            productUpdated.ProductName.ShouldBe(dto.ProductName);
+            productUpdated.Price.ShouldBe(dto.Price);
+            productUpdated.ProductKind.Id.ShouldBe(dto.ProductKind.Id);
+            productUpdated.ProductKind.ProductKindName.ShouldBe(dto.ProductKind.ProductKindName);
+        }
+
+        [Fact]
+        public async Task should_delete_product_kind_return_204_and_delete_from_db()
+        {
+            var product = await AddDefaultProduct();
+
+            var response = await Client.Request($"{Path}/{product.Id}").DeleteAsync();
+
+            response.StatusCode.ShouldBe((int)HttpStatusCode.NoContent);
+            var responseAfterDelete = await Client.Request($"{Path}/{product.Id}").AllowHttpStatus("404").GetAsync();
+            responseAfterDelete.StatusCode.ShouldBe((int)HttpStatusCode.NotFound);
+
+        }
+
+        [Fact]
+        public async Task should_get_product()
+        {
+            var product = await AddDefaultProduct();
+
+            var response = await Client.Request($"{Path}/{product.Id}").GetAsync();
+
+            response.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+            var dto = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductDetailsDto>();
+            dto.ShouldNotBeNull();
+            dto.ProductName.ShouldBe(product.ProductName);
+        }
+
+        [Fact]
+        public async Task should_get_products()
+        {
+            await AddDefaultProduct();
+            await AddDefaultProduct();
+            await AddDefaultProduct();
+
+            var response = await Client.Request($"{Path}").GetAsync();
+
+            response.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+            var dtos = await response.ResponseMessage.Content.ReadFromJsonAsync<IEnumerable<ProductDto>>();
+            dtos.ShouldNotBeEmpty();
+            dtos.Count().ShouldBe(3);
+        }
+
+        public async Task<ProductDetailsDto> AddDefaultProduct()
+        {
+            var productKindDto = _productKinds.First();
+            var dto = new ProductDetailsDto(0, $"Name{Guid.NewGuid():N}", productKindDto, 100);
+            var response = await Client.Request(Path).PostJsonAsync(dto);
+            response.StatusCode.ShouldBe((int)HttpStatusCode.Created);
+            var productAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductDetailsDto>();
+            return productAdded;
+        }
+
+        public async Task<ProductKindDto> AddDefaultProductKind()
+        {
+            var dto = new ProductKindDto(0, $"Name{Guid.NewGuid()}");
+            var response = await Client.Request("api/product-kinds").PostJsonAsync(dto);
+            response.StatusCode.ShouldBe((int)HttpStatusCode.Created);
+            var productKindAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
+            return productKindAdded;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _productKinds.Add(await AddDefaultProductKind());
+            _productKinds.Add(await AddDefaultProductKind());
+            _productKinds.Add(await AddDefaultProductKind());
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        private List<ProductKindDto> _productKinds = new();
+        private const string Path = "api/products";
+
+        public ProductController(OptionsProvider optionsProvider) : base(optionsProvider)
+        { }
+    }
+}
--- tests/MyOrders.IntegrationTests/Controllers/ProductKindControllerTests.cs
+++ tests/MyOrders.IntegrationTests/Controllers/ProductKindControllerTests.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Http;
 using MyOrders.Application.DTO;
 using MyOrders.IntegrationTests.Common;
 using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
@@ -20,6 +23,71 @@ namespace MyOrders.IntegrationTests.Controllers
             response.StatusCode.ShouldBe(StatusCodes.Status201Created);
             var productKindAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
             productKindAdded.ProductKindName.ShouldBe(dto.ProductKindName);
+        }
+
+        [Fact]
+        public async Task should_update_product_kind_and_return_200()
+        {
+            var addDto = new ProductKindDto(0, "Name");
+            var response = await Client.Request(Path).PostJsonAsync(addDto);
+            var productKindAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
+            var dto = new ProductKindDto(productKindAdded.Id, "ABCD");
+
+            var productKindUpdatedResponse = await Client.Request($"{Path}/{productKindAdded.Id}").PutJsonAsync(dto);
+
+            productKindUpdatedResponse.StatusCode.ShouldBe(StatusCodes.Status200OK);
+            var productKindUpdated = await productKindUpdatedResponse.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
+            productKindUpdated.ProductKindName.ShouldBe(dto.ProductKindName);
+        }
+
+        [Fact]
+        public async Task should_delete_product_kind_return_204_and_delete_from_db()
+        {
+            var productKind = await AddDefaultProductKind();
+
+            var response = await Client.Request($"{Path}/{productKind.Id}").DeleteAsync();
+
+            response.StatusCode.ShouldBe(StatusCodes.Status204NoContent);
+            var responseAfterDelete = await Client.Request($"{Path}/{productKind.Id}").AllowHttpStatus("404").GetAsync();
+            responseAfterDelete.StatusCode.ShouldBe(StatusCodes.Status404NotFound);
+
+        }
+
+        [Fact]
+        public async Task should_get_product_kind()
+        {
+            var productKind = await AddDefaultProductKind();
+
+            var response = await Client.Request($"{Path}/{productKind.Id}").GetAsync();
+
+            response.StatusCode.ShouldBe(StatusCodes.Status200OK);
+            var dto = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
+            dto.ShouldNotBeNull();
+            dto.ProductKindName.ShouldBe(productKind.ProductKindName);
+        }
+
+        [Fact]
+        public async Task should_get_product_kinds()
+        {
+            await AddDefaultProductKind();
+            await AddDefaultProductKind();
+            await AddDefaultProductKind();
+
+            var response = await Client.Request($"{Path}").GetAsync();
+
+            response.StatusCode.ShouldBe(StatusCodes.Status200OK);
+            var dtos = await response.ResponseMessage.Content.ReadFromJsonAsync<IEnumerable<ProductKindDto>>();
+            dtos.ShouldNotBeEmpty();
+            dtos.Count().ShouldBe(3);
+        }
+
+        public async Task<ProductKindDto> AddDefaultProductKind()
+        {
+            var dto = new ProductKindDto(0, $"Name{Guid.NewGuid()}");
+            var response = await Client.Request(Path).PostJsonAsync(dto);
+            response.StatusCode.ShouldBe(StatusCodes.Status201Created);
+            var productKindAdded = await response.ResponseMessage.Content.ReadFromJsonAsync<ProductKindDto>();
+            return productKindAdded;
         }
 
         private const string Path = "api/product-kinds";
